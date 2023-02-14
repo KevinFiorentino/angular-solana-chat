54c064ecfa40b5f36113d70e6482aba0dc6dec0e
Kevin <kevinfiorentino.1294@gmail.com>
2023-02-14 16:37:18 -0300

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule),
-  },
-  {
     path: 'chat',
     loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule),
   },
   {
     path: 'my-messages',
     loadChildren: () => import('./modules/my-messages/my-messages.module').then(m => m.MyMessagesModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'chat'
   },
 ];
 
