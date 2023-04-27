149199d6295dabfd4963d8a132beadb778d71b6b
Kevin <kevinfiorentino.1294@gmail.com>
2023-04-27 19:45:38 -0300

    UX deeplink improved

--- src/app/modules/redirects/components/phantom-redirect/phantom-redirect.component.ts
+++ src/app/modules/redirects/components/phantom-redirect/phantom-redirect.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { PhantomDeeplinkService } from '@shared/services/phantom/phantom-deeplink.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-phantom-redirect',
@@ -10,25 +9,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class PhantomRedirectComponent implements OnInit {
 
-  /*
+  public txId?: string;
 
-    TEST CONNECTION
-
-    publickKey=FHs4jGrYzBubjqZ2fHxH1wHNmY4v6r3oGbsskZEgiW35
-    privateKey=4JAWZUYwQJ8WhRe6PrnZmGT7cPPnKQ7Bk5qfyuHcuCPH
-
-    phantom_encryption_public_key=9KWwz3fUWtstbPCEu2MihBMo2dKVhyZC9qaDRYWFMwTZ
-    nonce=Fc1c7NLzxBS8MESbNBjRGw1bvnLyKJpUc
-    data=2ADoUFbVPP5pfanHcKCFNzgvg4Vw51B48Td6qBxdpNp7wCCPCr77HU7YzU38RQCmPiTb1RWkhnGZkp5dA5V3GqwX6AGKKmSorJcy1phwNDhRhWGqE2aN1JTacsPMYKMXZtGsdaWc1hYdoWAwXUKTsaSvzAQcPLkiz7LkY4yzY258LR6wDWd7c4EbvcvmdVfH2v6aWNE7w467dD9DHN2T5URsMAzTK5tFAErEUv13yVZFG46vZPcpSrF3EUsUgDHVCKhCiLYoYU4MYQJAKEN47HFBY45uQzxcj4pMMvCqE671WnkwHPGPGM7Jo6RfJwf1kmCpB66jWNBc5wmorzyE1S8qqowf4yDt2kDkz1R2QTb4gvqAwuqu36Mak2LbnUa6vqcAKFr1iq5NHsZoLjg2n54keVW4DWb2wxviuFGH7YvFuYb9kniRxR3QQRymaEsk5Ksj2J5Ng
-
-    HeDn...AL3z
-
-  */
+  public errorCode?: string;
+  public errorMessage?: string;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private snackBar: MatSnackBar,
     private phantomDeeplink: PhantomDeeplinkService
   ) { }
 
@@ -42,9 +30,12 @@ export class PhantomRedirectComponent implements OnInit {
 
     this.route.queryParams
       .subscribe((params: Params) => {
-        setTimeout(() => {
-          this.checkTypeRedirect(method, params);
-        }, 0);
+        if (!params.errorCode && !params.errorMessage)
+          setTimeout(() => { this.checkTypeRedirect(method, params); }, 0);
+        else {
+          this.errorCode = params.errorCode;
+          this.errorMessage = params.errorMessage;
+        }
       });
   }
 
@@ -54,7 +45,7 @@ export class PhantomRedirectComponent implements OnInit {
         this.onConnect(params);
       };break;
       case 'disconnect' : {
-        this.onDisconnect(params);
+        this.onDisconnect();
       };break;
       case 'signAndSendTransaction' : {
         this.onSignAndSendTransaction(params);
@@ -84,11 +75,14 @@ export class PhantomRedirectComponent implements OnInit {
       nonce,
       data,
     );
+
+    this.router.navigate(['/chat']);
   }
 
-  onDisconnect(params: Params): void {
+  onDisconnect(): void {
     // https://docs.phantom.app/phantom-deeplinks/provider-methods/disconnect
-    // It is necessary do nothing. Maybe redirect the user to another page.
+    // It is necessary do nothing.
+    this.router.navigate(['/chat']);
   }
 
   onSignAndSendTransaction(params: Params): void {
@@ -105,20 +99,13 @@ export class PhantomRedirectComponent implements OnInit {
       data,
     );
 
-    this.snackBar.open(`Transaction ID: ${txId}`, 'Close', {
-      duration: 7000,
-      horizontalPosition: 'right',
-      verticalPosition: 'bottom',
-      panelClass: ['app-alert-success']
-    });
+    // Depending the action, it will be necessary redirect the user to diferents pages
+    // Here, we only show the transaction ID on the HTML
+    this.txId = txId;
   }
 
   onSignMessage(params: Params): void {
     // https://docs.phantom.app/phantom-deeplinks/provider-methods/signmessage
-  }
-
-  onError(): void {
-
   }
 
 }
